270159481f4b74325f6d4657a9b3bbb1a89c18d5
John2013 <NaWasington@yandex.ru>
2018-01-19 21:28:09 +0300

    убрал загрузку контента из функций

--- coursera.py
+++ coursera.py
@@ -13,14 +13,14 @@ def get_web_content(url):
     return requests.get(url).content
 
 
-def get_random_courses_list(url, courses_amount):
-    xml_root = etree.XML(get_web_content(url))
+def get_random_courses_list(courses_list_xml, courses_amount):
+    xml_root = etree.XML(courses_list_xml)
     courses_list = [link.text for child in xml_root for link in child]
     return random.sample(courses_list, courses_amount)
 
 
-def get_course_info(course_link):
-    soup = BeautifulSoup(get_web_content(course_link), 'html.parser')
+def get_course_info(course_html):
+    soup = BeautifulSoup(course_html, 'html.parser')
     title = soup.select(
         '.rc-PhoenixCdpBanner .header-container .title'
     )[0].text
@@ -43,10 +43,11 @@ def output_courses_info_to_xlsx(courses_list, filepath='courses.xlsx'):
 if __name__ == '__main__':
     courses_count = 20
 
-    courses_links = get_random_courses_list(
-        'https://www.coursera.org/sitemap~www~courses.xml',
-        courses_count
+    courses_list_xml = get_web_content(
+        'https://www.coursera.org/sitemap~www~courses.xml'
     )
+
+    courses_links = get_random_courses_list(courses_list_xml, courses_count)
 
     if len(sys.argv) > 1:
         filepath = sys.argv[1]
@@ -63,7 +64,8 @@ if __name__ == '__main__':
         ],
     ]
     for course_link in courses_links:
-        courses_list.append(get_course_info(course_link))
+        course_html = get_web_content(course_link)
+        courses_list.append(get_course_info(course_html))
 
     output_courses_info_to_xlsx(courses_list, filepath)
     print("Готово!\nРезультат в файле:\n{}".format(realpath(filepath)))
